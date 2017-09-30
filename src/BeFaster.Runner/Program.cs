6556a97584917c6aaaf6c117c43d5e33d8745511
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2017-09-30 22:56:58 +0300

    Init solution

---
+++ src/BeFaster.Runner/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BeFaster.Runner
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Running the system:
+        /// ...
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
